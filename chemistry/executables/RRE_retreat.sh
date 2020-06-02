5b321b83d3e95462bd0ffb4456d436e21d9bb2af
Mathilde DUTREUILH <mathilde.dutreuilh@ensta-paristech.fr>
2020-06-02 18:36:42 +0200

    files to enable chemistry calculus + first try to implement takayama s solution

---
+++ chemistry/executables/RRE_retreat.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Bienvenue dans le script de calculs chimiques !"
+
+# afficher les données thermo et chimiques intéressantes
+
+# Script d'automatisation du traitement des données MATLAB
+# pour l'évolution d'une particule lagragienne
+# Initialement développé pour les structures RRE
+
+# Se placer dans le bon répertoire, passé en argument
+
+# Récupérer tous les noms de fichier intéressants
+
+# Pour chaque fichier intéressant, faire
+
+    # copier le fichier VTIM dans VTIM.dat
+    # copier le ficher senk dans senk.inp
+
+    # exécuter snkvtim avec wine
+    # exécuter snkout avec wine
+
+    # copier t_T_ESP_Ther.dat dans le bon fichier
