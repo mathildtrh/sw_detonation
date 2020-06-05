9321ddd8f0c6a529daccdbc7c2f5e164912d633e
Mathilde DUTREUILH <mathilde.dutreuilh@ensta-paristech.fr>
2020-06-05 11:44:35 +0200

    first bash script to automatize post-treatment

---
+++ chemistry/RRE_lag_particles/H2_O2_conds.m
@@ -0,0 +1,36 @@
+%gas parameters
+    %names
+    name_I='2/3*H2+1/3*O2';
+    name_II='He';
+    %ratios of specific heat
+    gamma_I=1.4016; %slow material, first phase
+    gamma_II=1.667; %fast material, second phase
+    %molecular masses:
+    mu_I=2/3*2+1/3*16;
+    mu_II=4;
+    %Universal gas constant (kg.mol^-1.K^-1)
+    R_u=8.314;
+    %Phase I gas constant
+    R_I=R_u/(mu_I*10^-3);
+ 
+%Experimental conditions:
+    %Angle of incidence:
+    omega_deg=25;
+    omega_rad=omega_deg*pi/180;
+    %Incident wave pressure jump
+        %Cond Remy
+            xi=18.5;
+
+    %inital temperature:
+        %cond Remy
+        T0_I=600; %Phase I
+        T0_II=1138; %Phase II
+        
+    %initial pressure:
+        %Cond Remy
+        P0=101e3;
+
+    %initial specific volume:
+    Vm0_I=R_I*T0_I/P0;
+    %distance of shock to interface at t=0 (m):
+    experiment_dim=1e-2;
--- chemistry/executables/RRE_retreat.sh
+++ chemistry/executables/RRE_retreat.sh
@@ -1,23 +1,53 @@
 #!/bin/bash
 
-echo "Bienvenue dans le script de calculs chimiques !"
-
-# afficher les données thermo et chimiques intéressantes
-
 # Script d'automatisation du traitement des données MATLAB
 # pour l'évolution d'une particule lagragienne
 # Initialement développé pour les structures RRE
 
-# Se placer dans le bon répertoire, passé en argument
+echo "Bienvenue dans le script de calculs chimiques !"
+
+
+# Vérifier qu'on est dans le bon dossier
+# celui qui contient les .exe
+
+#check=`ls *.exe`
+#if [ -z $check ]; then
+#    echo "Pas de fichiers executable"
+#    break
+#fi
 
 # Récupérer tous les noms de fichier intéressants
+# dans le bon répertoire, passé en argument 1
+list_VTIM=`ls $1`
+
+# faire tourner l'interpreteur 
+echo -e "Execution de l'interpreteur CHEMKIN... \n"
+wine INTERP.exe
+# vérifier execution correcte dans chem.out
+tail -6 chem.out | head -1
+echo -e "\n\n"
 
 # Pour chaque fichier intéressant, faire
-
+for file in $list_VTIM; do
+    echo "-----------> " $file "  --- Traitement en cours --------- "
     # copier le fichier VTIM dans VTIM.dat
-    # copier le ficher senk dans senk.inp
+    cp ../vtim_matlab/$file VTIM.dat
+    cp ../senk_matlab/$file senk.inp
 
     # exécuter snkvtim avec wine
-    # exécuter snkout avec wine
+    wine snkvtim.exe
+    echo "snkvtim done"
 
+    # exécuter snkout avec wine
+    wine snkout.exe
+    echo "snkout done"
     # copier t_T_ESP_Ther.dat dans le bon fichier
+    cp t_T_ESP_Ther.dat ../output_chemkin/
+    mv ../output_chemkin/t_T_ESP_Ther.dat ../output_chemkin/$file.dat
+    echo "copy = ok"
+done
+
+#for i in ../output_chemkin/VTIM_*;do 
+#mv $i ../output_chemkin/out_${i#../output_chemkin/VTIM_}
+#done
+echo "Fin du programme !"
